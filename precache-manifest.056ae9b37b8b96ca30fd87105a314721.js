cb10aae828f653ace2fd487f32c774417c7986c9
chepa <mykola.chepkyi@youshido.com>
2020-10-08 21:59:31 +0300

    Updates

---
+++ precache-manifest.056ae9b37b8b96ca30fd87105a314721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c62e74341971d99024227676411820",
+    "url": "/Millionare/index.html"
+  },
+  {
+    "revision": "4687806cc09679f39ed5",
+    "url": "/Millionare/static/css/main.caddd313.chunk.css"
+  },
+  {
+    "revision": "c559eeb390154b9638ce",
+    "url": "/Millionare/static/js/2.61d284b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Millionare/static/js/2.61d284b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4687806cc09679f39ed5",
+    "url": "/Millionare/static/js/main.7c2a2b63.chunk.js"
+  },
+  {
+    "revision": "e3699a5ea7ab6811309b",
+    "url": "/Millionare/static/js/runtime-main.ac18d5d2.js"
+  }
+]);
